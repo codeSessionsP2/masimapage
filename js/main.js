24c5082c8b21ea52392eedd20a516e57b7505aad
smnwndrr <dev@slomosmo.de>
2016-01-05 23:39:55 +0100

    Outsourcing js into separate file

---
+++ js/main.js
@@ -0,0 +1,13 @@
+function playAudio() {
+    var myAudio = document.getElementById("mp3");
+    if( myAudio.paused ) {
+        myAudio.play();
+        document.getElementById("audioButton").style.backgroundImage = "url(img/pause.svg)";
+        myAudio.addEventListener('ended', function() {
+            document.getElementById("audioButton").style.backgroundImage = "url(img/play.svg)";
+        });
+    } else {
+        document.getElementById("audioButton").style.backgroundImage = "url(img/play.svg)";
+        myAudio.pause();
+    }
+}
